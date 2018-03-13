bc9842d47a1d468a22dfe0662ca6c8a9770d599e
KhushbooBaheti <khushboobaheti627@gmail.com>
2018-03-13 18:43:40 +0530

    Create basic fill ups

--- src/btp/demo.java
+++ src/btp/demo.java
@@ -3,9 +3,11 @@ package btp;
 
 
 import java.util.ArrayList;
+
 import java.util.Collection;
 import java.util.List;
-import java.io.StringReader;
+import java.io.*;
+import java.nio.charset.Charset;
 
 import edu.stanford.nlp.process.Tokenizer;
 import edu.stanford.nlp.process.TokenizerFactory;
@@ -14,13 +16,16 @@ import edu.stanford.nlp.process.DocumentPreprocessor;
 import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.ling.SentenceUtils;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 
 class demo {
 
-  /**
+		static String sub;
+		static String filename;
+  /*
    * The main method demonstrates the easiest way to load a parser.
    * Simply call loadModel and specify the path of a serialized grammar
    * model, which can be a file, a resource on the classpath, or even a URL.
@@ -34,16 +39,17 @@ class demo {
    */
   public static void main(String[] args) {
     String parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
-   
-    if (args.length > 0) {
+
+  /*  if (args.length > 0) {
       parserModel = args[0];
-    }
+    }*/
     LexicalizedParser lp = LexicalizedParser.loadModel(parserModel);
 
     if (args.length == 0) {
       demoAPI(lp);
     } else {
       String textFile = (args.length > 1) ? args[1] : args[0];
+      filename=textFile;
       demoDP(lp, textFile);
     }
   }
@@ -67,14 +73,15 @@ class demo {
     // to DocumentPreprocessor
     for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
       Tree parse = lp.apply(sentence);
-      parse.pennPrint();
-      System.out.println();
+     // parse.pennPrint();
+     // System.out.println();
 
       if (gsf != null) {
         GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-        Collection tdl = gs.typedDependenciesCCprocessed();
+        ArrayList<TypedDependency> tdl =(ArrayList) gs.typedDependenciesCCprocessed();
         System.out.println(tdl);
         System.out.println();
+        formatOutput(tdl);
       }
     }
   }
@@ -94,8 +101,8 @@ class demo {
     String[] sent = { "ram", "is", "an", "easy", "sentence", "." };
     List<CoreLabel> rawWords = SentenceUtils.toCoreLabelList(sent);
     Tree parse = lp.apply(rawWords);
-    parse.pennPrint();
-    System.out.println();
+    //parse.pennPrint();
+    //System.out.println();
 
     // This option shows loading and using an explicit tokenizer
     String sent2 = "ram is another sentence.";
@@ -114,7 +121,7 @@ class demo {
  
     System.out.println(tdl);
     System.out.println();
-    generateOutput(tdl);
+    formatOutput(tdl);
     // You can also use a TreePrint object to print trees and dependencies
     TreePrint tp = new TreePrint("penn,typedDependenciesCollapsed");
     //tp.printTree(parse);
@@ -122,12 +129,49 @@ class demo {
 
   private demo() {} // static methods only
 
-  static public void generateOutput( List<TypedDependency> tdl){
+  static public void formatOutput( List<TypedDependency> tdl){
 	  for(int i=0;i<tdl.size();i++){
 		  if(tdl.get(i).reln().getShortName().equals("nsubj")){
-			  System.out.println(tdl.get(i).dep());
+			  //System.out.println(convertToString(tdl.get(i).dep()));
+			  sub=convertToString(tdl.get(i).dep());
+			  generateOutput(filename);
 		  }
 	  }
 	  
+  }
+  static public String convertToString(IndexedWord word){
+	  String result=null;
+	  result=word.toString().split("/")[0];
+	  return result;
+  }
+  static public void generateOutput(String filename){
+	  String line;
+	  try {
+		InputStream fr=new FileInputStream(filename);
+		InputStreamReader isr=new InputStreamReader(fr,Charset.forName("UTF-8"));
+		BufferedReader br=new BufferedReader(isr);
+		try {
+			while((line=br.readLine())!=null){
+				String[] words=line.split(" ");
+				for(int i=0;i<words.length;i++){
+					if(words[i].equals(sub)){
+						System.out.print("--"+" ");
+					}
+					else{
+						System.out.print(words[i]+" ");
+					}
+					
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
   }
 }
