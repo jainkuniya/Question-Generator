48972e5dec9bda7ebc2f330aac725b3bfc522580
KhushbooBaheti <khushboobaheti627@gmail.com>
2018-04-06 20:18:19 +0530

    QApp: Create paragraph upload UI.

--- QApp/app/src/main/java/com/example/user/qapp/MainActivity.java
+++ QApp/app/src/main/java/com/example/user/qapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.user.qapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button proceed;
+    EditText copyText;
+    String copiedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        proceed=(Button)findViewById(R.id.bt_proceed);
+        copyText=(EditText)findViewById(R.id.et_copy_text);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.bt_proceed){
+            copiedText=copyText.getText().toString();
+            if(copiedText!=null){
+                Toast.makeText(this, "ohk", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
