c4687f8098abe807041a82d781df39e2fb4eb798
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-11 01:07:59 +0530

    QApp: Fix file generation is not visible.
    INTERNAL STORAGE means app internal storage,
    ENTERNAL refers to storage where file can be
    saved & visble in file manager either in
    internal storage or SD_CARD.

--- QApp/app/src/main/java/com/example/user/qapp/utils/FileGenerator.java
+++ QApp/app/src/main/java/com/example/user/qapp/utils/FileGenerator.java
@@ -1,6 +1,7 @@
 package com.example.user.qapp.utils;
 
 import android.content.Context;
+import android.os.Environment;
 
 import com.example.user.qapp.network.UploadFile;
 
@@ -16,13 +17,12 @@ public class FileGenerator {
     File path, file;
     Context context;
     String fileName;
-
     public FileGenerator(Context context) {
         this.context = context;
     }
 
     public void createFile(String data) throws IOException {
-        path = new File(context.getFilesDir(), "QApp");
+        path = new File(Environment.getExternalStorageDirectory(), "QApp");
         if (!path.exists()) {
             path.mkdir();
         }
@@ -31,12 +31,13 @@ public class FileGenerator {
         file = new File(path, fileName);
         FileWriter stream = new FileWriter(file);
         try {
-            stream.append(data);
+            stream.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             stream.close();
         }
+        FileOpen.openFile(context, file);
         // sendFile();
     }
 
@@ -45,4 +46,5 @@ public class FileGenerator {
         up.upload_file(file, fileName);
     }
 
+    ///data/user/0/com.example.user.qapp/files/QApp/352175070657246_1523387288744.txt
 }
