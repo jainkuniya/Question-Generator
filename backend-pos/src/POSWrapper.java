1e9b6cd8395117b4d34f8d0f0d4ff7fdc398ba78
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-10 19:51:33 +0530

    backend-pos: Handle command line arguments.

--- backend-pos/src/POSWrapper.java
+++ backend-pos/src/POSWrapper.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
@@ -9,8 +10,20 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 public class POSWrapper {
   
 	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.out.println("Invalid Params: Include input file path");
+			return;
+		}
+		// try to read file new FileReader("../backend-pos/sample-input.txt")
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(args[0]);
+		}catch (FileNotFoundException e) {
+			System.out.println("Invalid input file path");
+			return;
+		}
 	    MaxentTagger tagger = new MaxentTagger("../backend-pos/models/english-left3words-distsim.tagger");
-	    List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(new FileReader("../backend-pos/sample-input.txt")));
+	    List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(fileReader));
 	    for (List<HasWord> sentence : sentences) {
 	      List<TaggedWord> tSentence = tagger.tagSentence(sentence);
 	      System.out.println("\nStarting new line");
