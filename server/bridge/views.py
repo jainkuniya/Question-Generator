9aa8d52530780d699ec1e1d59ec023e60ae1b7b1
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-10 22:35:42 +0530

    server: Pass file path to backend-pos after uploading.

--- server/bridge/views.py
+++ server/bridge/views.py
@@ -11,6 +11,7 @@ from .forms import UploadFileForm
 
 SUCCESS_CODE = 1
 INVALID_REQUEST_CODE = -99
+INPUT_FILE_PATH = './bridge/uploads/'
 
 def index(request):
     return HttpResponse(subprocess.check_output(['java', '-Dfile.encoding=UTF-8', '-classpath', '../backend-pos/bin:../backend-pos/stanford-postagger-3.9.1.jar', 'POSWrapper', '../backend-pos/sample-input.txt']))
@@ -19,11 +20,13 @@ def index(request):
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
+        input_file_name = request.POST['title']
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'], request.POST['title'])
+            handle_uploaded_file(request.FILES['file'], input_file_name)
             data = {
                 'success': SUCCESS_CODE,
                 'message': 'Successfully uploaded',
+                'output': subprocess.check_output(['java', '-Dfile.encoding=UTF-8', '-classpath', '../backend-pos/bin:../backend-pos/stanford-postagger-3.9.1.jar', 'POSWrapper', INPUT_FILE_PATH + input_file_name])
             }
     else:
         data = {
@@ -33,6 +36,6 @@ def upload_file(request):
     return JsonResponse(data, safe=True)
 
 def handle_uploaded_file(f, file_name):
-    with open('./bridge/uploads/' + file_name, 'wb+') as new_file:
+    with open(INPUT_FILE_PATH + file_name, 'wb+') as new_file:
         for chunk in f.chunks():
             new_file.write(chunk)
