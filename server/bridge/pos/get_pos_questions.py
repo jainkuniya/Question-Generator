988c2b25c26ec757d4c7e55bbd977b5312ed0f23
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-04-22 16:33:18 +0530

    server pos: Generate pos true/false identify questions.

--- server/bridge/pos/get_pos_questions.py
+++ server/bridge/pos/get_pos_questions.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 
 from bridge.template.multiple_choice_question import generate_multiple_choice_question
 from .pos_abbrevations import POS_ABBREVATIONS
@@ -25,7 +26,6 @@ def get_pos_identify_questions(parsed_data, sentence):
         # get POS from map
         tag = POS_ABBREVATIONS.get(value[KEY_TAG])
         if tag is None:
-            print value[KEY_TAG]
             continue
         options = get_any_words_except_tag_from_sentence(parsed_data, value[KEY_TAG], 3)
         if (options == None):
@@ -37,7 +37,37 @@ def get_pos_identify_questions(parsed_data, sentence):
                         "In the following sentence identify " + tag + ".",
                          options.index(value[KEY_WORD]),
                          options,
-                         sentence
+                         sentence,
+                         ''
+                         )
+                    )
+    return questions
+
+def get_pos_identify_true_false_question_sentence(word, tag):
+    return "Is " + word + ", " + tag + " in the following sentence?"
+
+def get_pos_identify_true_false_questions(parsed_data, sentence):
+    questions = []
+    for key, value in parsed_data.iteritems():
+        # get POS from map
+        tag = POS_ABBREVATIONS.get(value[KEY_TAG])
+        if tag is None:
+            continue
+        options = ["True", "False"]
+        secret = random.randint(0,1)
+        question_sentence = get_pos_identify_true_false_question_sentence(value[KEY_WORD], tag)
+        if (secret == 1):
+            tag_list = list(POS_ABBREVATIONS.keys())
+            wrong_tag = tag_list[random.randint(0,len(tag_list)-1)]
+            question_sentence = get_pos_identify_true_false_question_sentence(value[KEY_WORD], POS_ABBREVATIONS.get(wrong_tag))
+        explanation = value[KEY_WORD] + " is " + tag
+        questions.append(
+                generate_multiple_choice_question(
+                         question_sentence,
+                         secret,
+                         options,
+                         sentence,
+                         explanation,
                          )
                     )
     return questions
@@ -46,6 +76,8 @@ def get_pos_questions_from_sentence(parsed_data, sentence):
     questions = []
     # get identify POS questions
     questions.extend(get_pos_identify_questions(parsed_data, sentence))
+    # get identify POS True, False questions
+    questions.extend(get_pos_identify_true_false_questions(parsed_data, sentence))
     return questions
 
 def get_pos_questions(parsed_data):
--- server/bridge/template/multiple_choice_question.py
+++ server/bridge/template/multiple_choice_question.py
@@ -1,12 +1,13 @@
-from .keys import KEY_QUESTION_TYPE, KEY_QUESTION, KEY_ANSWER, KEY_OPTIONS, KEY_COMPREHENSION
+from .keys import KEY_QUESTION_TYPE, KEY_QUESTION, KEY_ANSWER, KEY_OPTIONS, KEY_COMPREHENSION, KEY_EXPLANATION
 
 QUESTION_TYPE = 2
 
-def generate_multiple_choice_question(question, answer, options, comprehension):
+def generate_multiple_choice_question(question, answer, options, comprehension, explanation):
     return {
             KEY_QUESTION_TYPE: QUESTION_TYPE,
             KEY_QUESTION: question,
             KEY_ANSWER: answer,
             KEY_OPTIONS: options,
-            KEY_COMPREHENSION: comprehension
+            KEY_COMPREHENSION: comprehension,
+            KEY_EXPLANATION: explanation
         }
