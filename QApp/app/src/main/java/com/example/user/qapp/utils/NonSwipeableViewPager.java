55df85f2b66a25c69288e46a781161d3a2141113
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-05-05 16:52:28 +0530

    QApp: Fix view pager height issue.

--- QApp/app/src/main/java/com/example/user/qapp/utils/NonSwipeableViewPager.java
+++ QApp/app/src/main/java/com/example/user/qapp/utils/NonSwipeableViewPager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Scroller;
 
@@ -55,5 +56,23 @@ public class NonSwipeableViewPager extends ViewPager {
         public void startScroll(int startX, int startY, int dx, int dy, int duration) {
             super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/);
         }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        if (height != 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        }
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
