bf1c3cf961238c8b03e6eb23fa7199bed1b41478
Vishwesh Jainkuniya <gitvishwesh@gmail.com>
2018-05-05 18:57:00 +0530

    QApp: Fix app crash if paragraph is not entered.

--- QApp/app/src/main/java/com/example/user/qapp/activities/MainActivity.java
+++ QApp/app/src/main/java/com/example/user/qapp/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.user.qapp.R;
 import com.example.user.qapp.network.UploadFile;
@@ -40,28 +41,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-  @OnClick(R.id.bt_proceed)
+    @OnClick(R.id.bt_proceed)
     public void proceed(View view) {
+        copiedText = copyText.getText().toString();
+
+        if (copiedText.trim().length() == 0) {
+            Toast.makeText(context, "Please enter paragarh", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String fileName = FileNameGenerator.generateFromIEMIAndTimeStamp(context) + ".txt";
+        try {
+            File file = FileGenerator.createFile(context, copiedText,
+                    fileName);
+            Log.i("MainActivity", "File created");
+            //upload file
+            UploadFile up = new UploadFile();
+            up.uploadFile(this, file, fileName);
 
 
-
-                copiedText = copyText.getText().toString();
-                String fileName = FileNameGenerator.generateFromIEMIAndTimeStamp(context) + ".txt";
-                try {
-                    File file = FileGenerator.createFile(context, copiedText,
-                            fileName);
-                    Log.i("MainActivity", "File created");
-                    //upload file
-                    UploadFile up = new UploadFile();
-                    up.uploadFile(this,file, fileName);
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+
     }
+
+}
 
